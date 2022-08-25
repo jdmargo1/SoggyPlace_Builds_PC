255998055be9b208822878694ef5d142aacc8d9c
JJ Margolis <jdmargo1@uci.edu>
2022-08-25 14:07:29 -0700

    Build #2
    Created Binaural Build
    Fixed Menu Aspect

---
+++ SoggyPlace_Binaural_PC/SoggyPlace_Binaural_PC_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,328 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BPLAY_SANDMAN_MONO_BINAURAL = 2378649527U;
+        static const AkUniqueID BSTOP_SANDMAN_MONO_BINAURAL = 437979949U;
+        static const AkUniqueID PLAY_1SA_OBJ_TREE_BRIDGESQUEAKING = 857970658U;
+        static const AkUniqueID PLAY_1SA_OBJ_TREE_LEAVES_35S = 2637457105U;
+        static const AkUniqueID PLAY_1SA_OBJ_TREE_WIND_1M = 132516726U;
+        static const AkUniqueID PLAY_ENV_WATER_WATERFAR_90S_01 = 3694913665U;
+        static const AkUniqueID PLAY_ENV_WATER_WATERFAR_90S_02 = 3694913666U;
+        static const AkUniqueID PLAY_ENV_WATER_WATERFAR_90S_03 = 3694913667U;
+        static const AkUniqueID PLAY_ENV_Z4_WATER_CREEK = 4250087417U;
+        static const AkUniqueID PLAY_ENV_Z4_WATER_UNDERFOOTBRIDGE = 165306904U;
+        static const AkUniqueID PLAY_ENV_Z4_WATER_WATERFAR_90S = 2665104876U;
+        static const AkUniqueID PLAY_ENV_Z5_WATER_WATERFAR_90S = 3593542675U;
+        static const AkUniqueID PLAY_ENV_Z6_WATER_WATERFAR_90S = 2395000674U;
+        static const AkUniqueID PLAY_ENV_Z7_WATER_WATERFAR_90S = 1042387369U;
+        static const AkUniqueID PLAY_FIRE = 3015324718U;
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_MR_SANDMAN___ROOMAWARE = 3523734198U;
+        static const AkUniqueID PLAY_OBJ_CABININT_WIND_55S = 820723023U;
+        static const AkUniqueID PLAY_OBJ_SHIP_GROAN_1M = 3235641557U;
+        static const AkUniqueID PLAY_OBJ_SHIP_WATERONHULL_1M = 1682493237U;
+        static const AkUniqueID PLAY_OBJ_TOWER_ROPEHITTINGPOLE = 1062397283U;
+        static const AkUniqueID PLAY_OBJ_TOWER_ROPEHITTINGPOLE_01 = 298938711U;
+        static const AkUniqueID PLAY_OBJ_TOWER_WINDCABLES_1M = 1992300427U;
+        static const AkUniqueID PLAY_OBJ_TOWER_WINDCABLES_1M_01 = 687429887U;
+        static const AkUniqueID PLAY_OBJ_WATER_WATERNEAR_1M = 3738252512U;
+        static const AkUniqueID PLAY_OBJ_WATER_WATERNEARBUSY_45S = 1246158329U;
+        static const AkUniqueID PLAY_OBJ_Z4_FENCING_WIND_1 = 3578344478U;
+        static const AkUniqueID PLAY_OBJ_Z4_FENCING_WIND_2 = 3578344477U;
+        static const AkUniqueID PLAY_OBJ_Z4_TREEDRY = 3900373068U;
+        static const AkUniqueID PLAY_OBJ_Z4_TREEMID = 3649841629U;
+        static const AkUniqueID PLAY_OBJ_Z4_TREESMALL = 2363775094U;
+        static const AkUniqueID PLAY_OBJ_Z4_WATER_ONTRUCK = 3608223883U;
+        static const AkUniqueID PLAY_OBJ_Z5_CHURCHINT_WIND1_45S = 2077540165U;
+        static const AkUniqueID PLAY_OBJ_Z5_STRUCTURE_SMALL_1M = 277048758U;
+        static const AkUniqueID PLAY_OBJ_Z5_TREES_BIG_1M = 4243523223U;
+        static const AkUniqueID PLAY_OBJ_Z5_TREES_BIG_1M_01 = 2815423515U;
+        static const AkUniqueID PLAY_OBJ_Z5_TREES_SMALL_1M5S_01 = 3001515944U;
+        static const AkUniqueID PLAY_OBJ_Z5_TREES_SMALL_1M5S_02 = 3001515947U;
+        static const AkUniqueID PLAY_OBJ_Z5_WATERNEAR_LAGOON_1M = 1493142109U;
+        static const AkUniqueID PLAY_OBJ_Z5_WATERNEAR_LAGOON_1M_01 = 2343960057U;
+        static const AkUniqueID PLAY_OBJ_Z6_BUGBUSH = 3978407909U;
+        static const AkUniqueID PLAY_OBJ_Z6_BUGBUSH2 = 822601037U;
+        static const AkUniqueID PLAY_OBJ_Z6_GREENHOUSE_WIND_1M = 553828362U;
+        static const AkUniqueID PLAY_OBJ_Z6_MANORINT_WIND_50 = 631806726U;
+        static const AkUniqueID PLAY_OBJ_Z6_TREES_BIG_1M = 3696965242U;
+        static const AkUniqueID PLAY_OBJ_Z7_CABININT_WIND_55S = 2528094147U;
+        static const AkUniqueID PLAY_OBJ_Z7_TREES_DED_1M = 2539260290U;
+        static const AkUniqueID PLAY_OBJ_Z7_TREES_MID_1M = 652224113U;
+        static const AkUniqueID PLAY_OBJ_Z7_TREES_SMALL_1M = 4211095612U;
+        static const AkUniqueID PLAY_SANDMAN_MONO = 1892595306U;
+        static const AkUniqueID PLAY_SANDMAN_STEREO = 3832904427U;
+        static const AkUniqueID PLAY_STEREOBED_MASTER = 3701245108U;
+        static const AkUniqueID PLAYER_IS_INSIDE = 2805152020U;
+        static const AkUniqueID PLAYER_IS_OUTSIDE = 1030551261U;
+        static const AkUniqueID STOP_1SA_OBJ_TREE_BRIDGESQUEAKING = 2632082212U;
+        static const AkUniqueID STOP_1SA_OBJ_TREE_LEAVES_35S = 2866617503U;
+        static const AkUniqueID STOP_1SA_OBJ_TREE_WIND_1M = 2911216552U;
+        static const AkUniqueID STOP_ENV_WATER_WATERFAR_90S = 3649876163U;
+        static const AkUniqueID STOP_ENV_Z4_WATER_CREEK = 1076114459U;
+        static const AkUniqueID STOP_ENV_Z4_WATER_UNDERFOOTBRIDGE = 3398481246U;
+        static const AkUniqueID STOP_ENV_Z4_WATER_WATERFAR_90S = 1215292494U;
+        static const AkUniqueID STOP_ENV_Z5_WATER_WATERFAR_90S = 1053909221U;
+        static const AkUniqueID STOP_ENV_Z6_WATER_WATERFAR_90S = 922843144U;
+        static const AkUniqueID STOP_ENV_Z7_WATER_WATERFAR_90S = 3977408159U;
+        static const AkUniqueID STOP_MR_SANDMAN___ROOMAWARE = 3289786080U;
+        static const AkUniqueID STOP_OBJ_CABININT_WIND_55S = 2610888853U;
+        static const AkUniqueID STOP_OBJ_SHIP_GROAN_1M = 898560387U;
+        static const AkUniqueID STOP_OBJ_SHIP_WATERONHULL_1M = 990530763U;
+        static const AkUniqueID STOP_OBJ_TOWER_ROPEHITTINGPOLE = 71425473U;
+        static const AkUniqueID STOP_OBJ_TOWER_ROPEHITTINGPOLE_01 = 2886721037U;
+        static const AkUniqueID STOP_OBJ_TOWER_WINDCABLES_1M = 3693274533U;
+        static const AkUniqueID STOP_OBJ_TOWER_WINDCABLES_1M_01 = 56317057U;
+        static const AkUniqueID STOP_OBJ_WATER_WATERNEAR_1M = 3674338606U;
+        static const AkUniqueID STOP_OBJ_WATER_WATERNEARBUSY_45S = 389065783U;
+        static const AkUniqueID STOP_OBJ_Z4_FENCING_WIND_1 = 763342892U;
+        static const AkUniqueID STOP_OBJ_Z4_FENCING_WIND_2 = 763342895U;
+        static const AkUniqueID STOP_OBJ_Z4_TREEDRY = 3139178578U;
+        static const AkUniqueID STOP_OBJ_Z4_TREEMID = 3221639647U;
+        static const AkUniqueID STOP_OBJ_Z4_TREESMALL = 2134042796U;
+        static const AkUniqueID STOP_OBJ_Z4_WATER_ONTRUCK = 575486529U;
+        static const AkUniqueID STOP_OBJ_Z5_CHURCHINT_WIND1_45S = 3211172175U;
+        static const AkUniqueID STOP_OBJ_Z5_STRUCTURE_SMALL_1M = 4247022028U;
+        static const AkUniqueID STOP_OBJ_Z5_TREES_BIG_1M = 1221517833U;
+        static const AkUniqueID STOP_OBJ_Z5_TREES_BIG_1M_01 = 628327669U;
+        static const AkUniqueID STOP_OBJ_Z5_TREES_SMALL_1M5S_01 = 3723743562U;
+        static const AkUniqueID STOP_OBJ_Z5_TREES_SMALL_1M5S_02 = 3723743561U;
+        static const AkUniqueID STOP_OBJ_Z5_WATERNEAR_LAGOON_1M = 2071700963U;
+        static const AkUniqueID STOP_OBJ_Z5_WATERNEAR_LAGOON_1M_01 = 593188567U;
+        static const AkUniqueID STOP_OBJ_Z6_BUGBUSH = 3196361743U;
+        static const AkUniqueID STOP_OBJ_Z6_BUGBUSH2 = 4190219183U;
+        static const AkUniqueID STOP_OBJ_Z6_GREENHOUSE_WIND_1M = 2426146660U;
+        static const AkUniqueID STOP_OBJ_Z6_MANORINT_WIND_50 = 1361329412U;
+        static const AkUniqueID STOP_OBJ_Z6_TREES_BIG_1M = 1105764120U;
+        static const AkUniqueID STOP_OBJ_Z7_CABININT_WIND_55S = 3126902801U;
+        static const AkUniqueID STOP_OBJ_Z7_TREES_DED_1M = 3962409136U;
+        static const AkUniqueID STOP_OBJ_Z7_TREES_MID_1M = 141724511U;
+        static const AkUniqueID STOP_OBJ_Z7_TREES_SMALL_1M = 2833802126U;
+        static const AkUniqueID STOP_SANDMAN_MONO = 3218835252U;
+        static const AkUniqueID STOP_SANDMAN_STEREO = 3477306029U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace INDOOR_OUTDOOR
+        {
+            static const AkUniqueID GROUP = 145721553U;
+
+            namespace STATE
+            {
+                static const AkUniqueID INSIDE_ASYLUM = 2224442717U;
+                static const AkUniqueID INSIDE_BARN = 2760249547U;
+                static const AkUniqueID INSIDE_BRICKBUILDING = 1960431119U;
+                static const AkUniqueID INSIDE_CABIN = 1978035275U;
+                static const AkUniqueID INSIDE_CHURCH = 1592194961U;
+                static const AkUniqueID INSIDE_GREENHOUSE = 2662993391U;
+                static const AkUniqueID INSIDE_HALL = 669930569U;
+                static const AkUniqueID INSIDE_MANOR = 1215237523U;
+                static const AkUniqueID INSIDE_OFFICEBUILDING = 1379511336U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OUTSIDE = 438105790U;
+            } // namespace STATE
+        } // namespace INDOOR_OUTDOOR
+
+        namespace ZONE_STATE
+        {
+            static const AkUniqueID GROUP = 3530926665U;
+
+            namespace STATE
+            {
+                static const AkUniqueID A_STARTINGAREA = 2610041010U;
+                static const AkUniqueID B_SECRETZONE1 = 3769583171U;
+                static const AkUniqueID C_FRONTBAYOU = 233093088U;
+                static const AkUniqueID D_MAINSTREET = 795838972U;
+                static const AkUniqueID E_CHURCH = 2452388028U;
+                static const AkUniqueID F_MAINSTREETMANOR = 1918129569U;
+                static const AkUniqueID G_SEASIDESHACKS = 2134675750U;
+                static const AkUniqueID H_GOVERNMENTOFFICES = 2451872362U;
+                static const AkUniqueID I_VILLAGEMAIN = 379740444U;
+                static const AkUniqueID J_VILLAGEPARK = 83065352U;
+                static const AkUniqueID K_MANOR_ISLAND = 2043055754U;
+                static const AkUniqueID L_BACKBAYOU = 4250780145U;
+                static const AkUniqueID M_VIRIDIANFOREST = 1322290778U;
+                static const AkUniqueID N_SCHOOL = 1127321912U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID O_NATURERESERVE_ISLAND = 1491759694U;
+                static const AkUniqueID P_ASYLUM_ISLAND = 1532058053U;
+            } // namespace STATE
+        } // namespace ZONE_STATE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEPSWITCH
+        {
+            static const AkUniqueID GROUP = 361622075U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BRIDGEBIG = 826104432U;
+                static const AkUniqueID BRIDGESMALL = 3999594623U;
+                static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID GRAVEL = 2185786256U;
+                static const AkUniqueID MOSS = 2927316363U;
+                static const AkUniqueID MUD = 712897245U;
+                static const AkUniqueID TILE = 2637588553U;
+                static const AkUniqueID WOODFLOOR = 3111880208U;
+                static const AkUniqueID WOODFLOORROTTEN = 627471700U;
+                static const AkUniqueID WOODPATH = 268523767U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPSWITCH
+
+        namespace INDOOR_OUTDOOR
+        {
+            static const AkUniqueID GROUP = 145721553U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID INSIDE_ASYLUM = 2224442717U;
+                static const AkUniqueID INSIDE_BARN = 2760249547U;
+                static const AkUniqueID INSIDE_BRICKBUILDING = 1960431119U;
+                static const AkUniqueID INSIDE_CABIN = 1978035275U;
+                static const AkUniqueID INSIDE_CHURCH = 1592194961U;
+                static const AkUniqueID INSIDE_GREENHOUSE = 2662993391U;
+                static const AkUniqueID INSIDE_HALL = 669930569U;
+                static const AkUniqueID INSIDE_MANOR = 1215237523U;
+                static const AkUniqueID OUTSIDE = 438105790U;
+            } // namespace SWITCH
+        } // namespace INDOOR_OUTDOOR
+
+        namespace PLAYER_SPEED
+        {
+            static const AkUniqueID GROUP = 1062779386U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID RUNNING = 3863236874U;
+                static const AkUniqueID SPRINTING = 3691594375U;
+                static const AkUniqueID WALKING = 340271938U;
+            } // namespace SWITCH
+        } // namespace PLAYER_SPEED
+
+        namespace ZONE_SWITCH
+        {
+            static const AkUniqueID GROUP = 2145492682U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID A_STARTINGAREA = 2610041010U;
+                static const AkUniqueID B_SECRETZONE1 = 3769583171U;
+                static const AkUniqueID C_FRONTBAYOU = 233093088U;
+                static const AkUniqueID D_MAINSTREET = 795838972U;
+                static const AkUniqueID E_CHURCH = 2452388028U;
+                static const AkUniqueID F_MAINSTREETMANOR = 1918129569U;
+                static const AkUniqueID G_SEASIDESHACKS = 2134675750U;
+                static const AkUniqueID H_GOVERNMENTOFFICES = 2451872362U;
+                static const AkUniqueID I_VILLAGEMAIN = 379740444U;
+                static const AkUniqueID J_VILLAGEPARK = 83065352U;
+                static const AkUniqueID K_MANOR_ISLAND = 2043055754U;
+                static const AkUniqueID L_BACKBAYOU = 4250780145U;
+                static const AkUniqueID M_VIRIDIANFOREST = 1322290778U;
+                static const AkUniqueID N_SCHOOL = 1127321912U;
+                static const AkUniqueID O_NATURERESERVE_ISLAND = 1491759694U;
+                static const AkUniqueID P_ASYLUM_ISLAND = 1532058053U;
+            } // namespace SWITCH
+        } // namespace ZONE_SWITCH
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID DISTANCETOPLAYER = 448976980U;
+        static const AkUniqueID FEAR = 3133847859U;
+        static const AkUniqueID FOG = 1083137563U;
+        static const AkUniqueID INDOOROUTDOORRTPC = 245748513U;
+        static const AkUniqueID PLAYERSPEED = 1493153371U;
+        static const AkUniqueID ZONERTPC = 471239826U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID A_STARTINGAREA = 2610041010U;
+        static const AkUniqueID B_SECRETZONE1 = 3769583171U;
+        static const AkUniqueID C_FRONTBAYOU = 233093088U;
+        static const AkUniqueID D_MAINSTREET = 795838972U;
+        static const AkUniqueID E_CHURCH = 2452388028U;
+        static const AkUniqueID F_MAINSTREETMANOR = 1918129569U;
+        static const AkUniqueID G_SEASIDESHACKS = 2134675750U;
+        static const AkUniqueID H_GOVERNMENTOFFICES = 2451872362U;
+        static const AkUniqueID I_VILLAGEMAIN = 379740444U;
+        static const AkUniqueID J_VILLAGEPARK = 83065352U;
+        static const AkUniqueID K_MANOR_ISLAND = 2043055754U;
+        static const AkUniqueID L_BACKBAYOU = 4250780145U;
+        static const AkUniqueID M_VIRIDIANFOREST = 1322290778U;
+        static const AkUniqueID MAIN = 3161908922U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID N_SCHOOL = 1127321912U;
+        static const AkUniqueID O_NATURERESERVE_ISLAND = 1491759694U;
+        static const AkUniqueID P_ASYLUM_ISLAND = 1532058053U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID PLAYER = 1069431850U;
+        static const AkUniqueID REVERB_ENVIRONMENTS = 3899103154U;
+        static const AkUniqueID ST_BEDS = 414403015U;
+        static const AkUniqueID STRUCTURES_EXT = 523453215U;
+        static const AkUniqueID STRUCTURES_INT = 354985509U;
+        static const AkUniqueID TREES = 3882419280U;
+        static const AkUniqueID WATER = 2654748154U;
+        static const AkUniqueID WORLD_BINAURAL = 1091113804U;
+        static const AkUniqueID WORLD_STEREO = 2138983586U;
+        static const AkUniqueID ZTESTREVERB_ENVIRONMENTS = 1575162390U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID CABIN = 680291572U;
+        static const AkUniqueID EARLYREFLECTIONSBUS = 299979518U;
+        static const AkUniqueID ENV_ASYLUM = 4032215156U;
+        static const AkUniqueID ENV_BARN = 443317174U;
+        static const AkUniqueID ENV_BRICKBUILDING = 189744232U;
+        static const AkUniqueID ENV_CABIN = 1414731772U;
+        static const AkUniqueID ENV_CHURCH = 2931536880U;
+        static const AkUniqueID ENV_EXTERIOR = 4099615387U;
+        static const AkUniqueID ENV_GREENHOUSE = 3450977802U;
+        static const AkUniqueID ENV_HALL = 1722576708U;
+        static const AkUniqueID ENV_MANOR = 103915856U;
+        static const AkUniqueID ENV_MANOR_BATHROOM = 1895941593U;
+        static const AkUniqueID ENV_OUTSIDE1 = 125853923U;
+        static const AkUniqueID ENV_OUTSIDE2 = 125853920U;
+        static const AkUniqueID ENV_ROOM_BALL = 634216916U;
+        static const AkUniqueID ENV_ROOM_HALL = 3884708126U;
+        static const AkUniqueID EXTERIOR_BINAURAL = 1553636952U;
+        static const AkUniqueID MANOR_BATHROOM_BINAURAL = 2065485210U;
+        static const AkUniqueID MANOR_BINAURAL = 3026171137U;
+        static const AkUniqueID MANOR_SECONDFLOOR_BINAURAL = 1438654516U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
